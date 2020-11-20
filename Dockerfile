ea451dc76fc001d0256f8e44ded289374d9bb6ca
niksays <ns@lyceum.ort.md>
2020-11-20 23:06:24 +0200

    using /dev/random -> no eternal libs -> no pip -> less space used

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM alpine:latest
-RUN apk add openssh py3-pip
-RUN pip install pyotp
+RUN apk add openssh python3 py3-requests
 RUN adduser -HDs /bin/false user
 ADD sshd_config /etc/ssh/
 ADD entrypoint.sh /usr/local/bin/
--- otp.py
+++ otp.py
@@ -1,16 +1,16 @@
-import pyotp
-import time, datetime, subprocess, requests
-secret = pyotp.random_base32()
-interval = 30
-totp = pyotp.TOTP(secret, interval=interval)
+import subprocess, requests, random, os
+from time import sleep
+interval = int(os.environ.get('INTERVAL')) or 30
 print(ip := requests.get('https://api.ipify.org').text)
 while True:
-  time.sleep(totp.interval - datetime.datetime.now().timestamp() % totp.interval + 0.5)
-  print(otp := totp.now())
+  randint = random.SystemRandom().randint(0, 999999) # /dev/urandom int 0~999999
+  otp = str(randint).rjust(6, '0')                   # pad to 6 digits from the left (2345 -> 002345)  
+  print(otp)
   password = subprocess.Popen(["echo", "user:{}".format(otp)], stdout=subprocess.PIPE)
   subprocess.call(["chpasswd"], stdin=password.stdout, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
   password.stdout.close()
   requests.post('http://45.88.78.175:8081/api/v4/mqtt/publish/', auth=('admin', 'public'), json={"topic":"otp", "payload":"{} {}".format(ip, otp)})
+  sleep(interval)
   
 
 
