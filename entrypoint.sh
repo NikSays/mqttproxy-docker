0b6441b7eb2753a850179e63c38b00c7e58abd92
niksays <ns@lyceum.ort.md>
2020-11-22 14:25:31 +0200

    Silence fail2ban startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ if [[ ! -f /opt/keys/ssh_host_rsa_key  ]]; then
 fi
 /sbin/syslogd
 /usr/sbin/sshd
-/usr/bin/fail2ban-server
-/usr/bin/fail2ban-client set sshd bantime ${BAN_TIME-12h}
-/usr/bin/fail2ban-client set sshd maxretry ${BAN_ATTEMPTS-5} 
-/usr/bin/fail2ban-client set sshd findtime ${BAN_FIND_INTERVAL-10m} 
+/usr/bin/fail2ban-server 1> /dev/null
+/usr/bin/fail2ban-client set sshd bantime ${BAN_TIME-12h} 1> /dev/null
+/usr/bin/fail2ban-client set sshd maxretry ${BAN_ATTEMPTS-5} 1> /dev/null
+/usr/bin/fail2ban-client set sshd findtime ${BAN_FIND_INTERVAL-10m} 1> /dev/null
 python3 -u /opt/otp.py
