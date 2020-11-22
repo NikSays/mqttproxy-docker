aa0d58a8f1f289ac0c8cbcea5070d24a4b996138
niksays <ns@lyceum.ort.md>
2020-11-22 11:46:02 +0200

    quiet key creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [[ ! -f /opt/keys/ssh_host_rsa_key  ]]; then
-  ssh-keygen -t rsa -f /opt/keys/ssh_host_rsa_key -N ""
-  ssh-keygen -t ecdsa -f /opt/keys/ssh_host_ecdsa_key -N ""
-  ssh-keygen -t ed25519 -f /opt/keys/ssh_host_ed25519_key -N ""
+  ssh-keygen -q -t rsa -f /opt/keys/ssh_host_rsa_key -N ""
+  ssh-keygen -q -t ecdsa -f /opt/keys/ssh_host_ecdsa_key -N ""
+  ssh-keygen -q -t ed25519 -f /opt/keys/ssh_host_ed25519_key -N ""
 fi
 /usr/sbin/sshd
 python3 -u /opt/otp.py
