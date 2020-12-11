1cd4aea9256fee79fd123b54f9fecd18b76cb765
niksays <ns@lyceum.ort.md>
2020-12-12 01:18:12 +0200

    new format of data

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ RUN adduser -HDs /bin/false user
 ADD sshd_config /etc/ssh/
 ADD entrypoint.sh /usr/local/bin/
 ADD otp.py /opt/
-ENTRYPOINT /usr/local/bin/entrypoint.sh
+CMD /usr/local/bin/entrypoint.sh
--- otp.py
+++ otp.py
@@ -1,20 +1,30 @@
 import subprocess, requests, random, os
 from time import sleep
-interval = int(os.environ.get('INTERVAL')) or 30
-print(ip := requests.get('https://api.ipify.org').text)
+try:
+  interval = int(os.environ.get('INTERVAL'))
+  assert interval > 5 and interval < 120
+except:
+  print('INTERVAL isn\'t an int between 5 and 120 seconds, defaulting to 30')
+  interval = 30
+  
+print(posData := requests.get('http://ip-api.com/json/?fields=status,country,city,lat,lon,query').json())
+assert posData['status'] == 'success', 'unable to get positional data'
+
+posData.pop('status')
+
+payload = {
+  'ip': posData.pop('query'),
+  'interval': interval,
+  'pos': {}  
+} 
+
+payload['pos'] = posData 
+
 while True:
   randint = random.SystemRandom().randint(0, 999999) # /dev/urandom int 0~999999
   otp = str(randint).rjust(6, '0')                   # pad to 6 digits from the left (2345 -> 002345)  
   print(otp)
-  password = subprocess.Popen(["echo", "user:{}".format(otp)], stdout=subprocess.PIPE)
-  subprocess.call(["chpasswd"], stdin=password.stdout, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-  password.stdout.close()
-  requests.post('http://45.88.78.175:8081/api/v4/mqtt/publish/', auth=('admin', 'public'), json={"topic":"otp", "payload":"{} {}".format(ip, otp)})
+  password = subprocess.Popen(f"echo user:{otp} | chpasswd", shell=True)
+ 
+  requests.post('http://45.88.78.175:8081/api/v4/mqtt/publish/', auth=('admin', 'public'), json={"topic":"otp", "payload":{**payload, 'otp':otp}})
   sleep(interval)
-  
-
-
-
-
-
-  
